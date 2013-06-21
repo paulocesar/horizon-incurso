0671ca3fe5252ed22879e70c4874c75a288b7bd5
Paulo Cesar <pauloc062@gmail.com>
2013-06-21 01:22:04 -0300

    moving invite policy to the right place

--- app/policy.js
+++ app/policy.js
@@ -1,20 +1,27 @@
 module.exports = {
+  
   SessionController : {
     logout : 'auth'
   },
+  
   UserController : {
     index : 'auth_admin',
     create : 'auth_admin',
     edit : 'auth_admin',
     delete : 'auth_admin'
   },
+  
   ChannelController : {
     index : 'auth',
     view : 'auth_channel_view',
     create : 'auth_manager',
     edit : 'auth_manager',
-    delete : 'auth_channel_delete',
+    delete : 'auth_channel_delete'
+  },
+
+  ChannelUserController : {
     invite : 'auth_manager',
     uninvite : 'auth_manager'
-  },
+  }
+
 };
